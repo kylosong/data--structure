6f4c5bc9b6261c1ddc7a82513df2a48f82588737
kylosong <1606760273@qq.com>
2023-12-29 22:03:53 +0800

    update

--- src/practice/day07/practice01/SuccessorNode.java
+++ src/practice/day07/practice01/SuccessorNode.java
@@ -1,12 +1,12 @@
 package practice.day07.practice01;
 
 public class SuccessorNode {
-    public static void main(String[] args) {
-        // 二叉树结构定义如下：给你二叉树中的某个节点，返回该节点的后继节点。
-        //（后继节点指的是一棵二叉树中，在中序遍历的序列中，一个节点的下一个节点），
-        // 要求时间复杂度O(K)不是O(N)，空间复杂度O(N)，而不是O(2n平方 - 1)
+	public static void main(String[] args) {
+		// 二叉树结构定义如下：给你二叉树中的某个节点，返回该节点的后继节点。
+		// （后继节点指的是一棵二叉树中，在中序遍历的序列中，一个节点的下一个节点），
+		// 要求时间复杂度O(K)不是O(N)，空间复杂度O(N)，而不是O(2n平方 - 1)
 
-        Node head = new Node(6);
+		Node head = new Node(6);
 		head.parent = null;
 		head.left = new Node(3);
 		head.left.parent = head;
@@ -26,9 +26,9 @@ public class SuccessorNode {
 		head.right.left.left.parent = head.right.left;
 		head.right.right = new Node(10);
 		head.right.right.parent = head.right;
-    }
+	}
 
-    public static class Node {
+	public static class Node {
 		public int value;
 		public Node left;
 		public Node right;
